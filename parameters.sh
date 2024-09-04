d5fc3cd488a39e90bdb286688e1a59acb7879bb9
Krzysztof Sikora <krzysztof.sikora@nordcloud.com>
2024-09-04 15:46:13 +0200

    new

---
+++ parameters.sh
@@ -0,0 +1,26 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://d-9967604275.awsapps.com/start
+REGION=eu-central-1
+TEAM_ACCOUNT=296062590666
+ORG_MASTER_PROFILE=orgMgmtAcc
+TEAM_ACCOUNT_PROFILE=teamDeplAcc
+TEAM_ADMIN_GROUP="TEAM admin"
+TEAM_AUDITOR_GROUP="TEAM auditors"
+TAGS="project=iam-identity-center-team environment=prod"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:eu-central-1:864899864501:eventdatastore/33995010-61ca-42fb-ba36-7d24d901d662
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
